68d6bb216ae1e966b68c5b6c2fa9f82c65c8325b
HenriqueHartmann <alemaohartmann@gmail.com>
2022-01-10 11:16:27 -0300

    add new functionalities to the dashboard

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:bytebank/screens/contatcts_list.dart';
+import 'package:bytebank/screens/contacts_list.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -14,41 +14,67 @@ class Dashboard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(16.0),
             child: Image.asset('images/bytebank_logo.png'),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Material(
-              color: Theme.of(context).primaryColor,
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(
-                          builder: (context) => const ContactsList()
-                      )
-                  );
-                },
-                child: Container(
-                    padding: const EdgeInsets.all(8.0),
-                    width: 150,
-                    height: 100,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const <Widget>[
-                        Icon(Icons.people, color: Colors.white, size: 24.0,),
-                        Text('Contacts', style: TextStyle(color: Colors.white,
-                            fontSize: 16.0),)
-                      ],
-                    )
-                ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GridView.count(
+                // horizontal spacing between the items
+                crossAxisSpacing: 4,
+                // vertical spacing between the items
+                mainAxisSpacing: 2,
+                // number of items per row
+                crossAxisCount: 3,
+                children: const <Widget>[
+                  _FeatureItem(icon: Icons.people, name: 'Contacts'),
+                  _FeatureItem(icon: Icons.monetization_on, name: 'Transfer'),
+                  _FeatureItem(icon: Icons.description, name: 'Transaction Feed'),
+                ],
               ),
             ),
-
           ),
         ],
       ),
+    );
+  }
+}
+
+class _FeatureItem extends StatelessWidget {
+  const _FeatureItem({Key? key, required this.icon, required this.name}) : super(key: key);
+
+  final IconData icon;
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Theme.of(context).primaryColor,
+      borderRadius: const BorderRadius.all(Radius.circular(5)),
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(
+                    builder: (context) => const ContactsList()
+                )
+            );
+          },
+          child: Container(
+              padding: const EdgeInsets.all(8.0),
+              width: 150,
+              height: 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Icon(icon, color: Colors.white, size: 24.0,),
+                  Text(name, style: const TextStyle(color: Colors.white,
+                      fontSize: 16.0),)
+                ],
+              )
+          ),
+        ),
     );
   }
 }
