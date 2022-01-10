313ee13534567fcb8a3cdeac2ec9508b3ffca83d
HenriqueHartmann <alemaohartmann@gmail.com>
2022-01-10 11:45:27 -0300

    delegate events with callbacks

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -27,10 +27,27 @@ class Dashboard extends StatelessWidget {
                 mainAxisSpacing: 2,
                 // number of items per row
                 crossAxisCount: 3,
-                children: const <Widget>[
-                  _FeatureItem(icon: Icons.people, name: 'Contacts'),
-                  _FeatureItem(icon: Icons.monetization_on, name: 'Transfer'),
-                  _FeatureItem(icon: Icons.description, name: 'Transaction Feed'),
+                children: <Widget>[
+                  _FeatureItem(
+                      icon: Icons.people,
+                      name: 'Contacts',
+                      onClick: () {
+                        _showContactsList(context);
+                      }),
+                  _FeatureItem(
+                    icon: Icons.monetization_on,
+                    name: 'Transfer',
+                    onClick: () {
+                      print('ok');
+                    },
+                  ),
+                  _FeatureItem(
+                    icon: Icons.description,
+                    name: 'Transaction Feed',
+                    onClick: () {
+                      print('ok');
+                    },
+                  ),
                 ],
               ),
             ),
@@ -41,40 +58,47 @@ class Dashboard extends StatelessWidget {
   }
 }
 
+void _showContactsList(BuildContext context) {
+  Navigator.of(context)
+      .push(MaterialPageRoute(builder: (context) => const ContactsList()));
+}
+
 class _FeatureItem extends StatelessWidget {
-  const _FeatureItem({Key? key, required this.icon, required this.name}) : super(key: key);
+  const _FeatureItem(
+      {Key? key, required this.icon, required this.name, required this.onClick})
+      : super(key: key);
 
   final IconData icon;
   final String name;
+  final Function? onClick;
 
   @override
   Widget build(BuildContext context) {
     return Material(
-        color: Theme.of(context).primaryColor,
+      color: Theme.of(context).primaryColor,
       borderRadius: const BorderRadius.all(Radius.circular(5)),
-        child: InkWell(
-          onTap: () {
-            Navigator.of(context).push(
-                MaterialPageRoute(
-                    builder: (context) => const ContactsList()
+      child: InkWell(
+        onTap: () => onClick!(),
+        child: Container(
+            padding: const EdgeInsets.all(8.0),
+            width: 150,
+            height: 100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+                Text(
+                  name,
+                  style: const TextStyle(color: Colors.white, fontSize: 16.0),
                 )
-            );
-          },
-          child: Container(
-              padding: const EdgeInsets.all(8.0),
-              width: 150,
-              height: 100,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Icon(icon, color: Colors.white, size: 24.0,),
-                  Text(name, style: const TextStyle(color: Colors.white,
-                      fontSize: 16.0),)
-                ],
-              )
-          ),
-        ),
+              ],
+            )),
+      ),
     );
   }
 }
