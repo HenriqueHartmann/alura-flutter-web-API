b62eb6d57be574421d3f29ffc7a5e0066971cb11
HenriqueHartmann <alemaohartmann@gmail.com>
2022-01-18 12:06:18 -0300

    use http interceptor

--- lib/http/web_client.dart
+++ lib/http/web_client.dart
@@ -1,9 +1,28 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    print(data);
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    print(data);
+    return data;
+  }
+}
 
 void findAll() async {
   const String endpoint = 'http://172.16.83.104:8080';
   const String api = '/transactions';
 
-  final Response response = await get(Uri.parse(endpoint + api));
-  print(response.body);
+  final Client client = InterceptedClient.build(
+      interceptors: [LoggingInterceptor()],
+      requestTimeout: const Duration(seconds: 5)
+  );
+
+  final Response response = await client.get(Uri.parse(endpoint + api));
 }
